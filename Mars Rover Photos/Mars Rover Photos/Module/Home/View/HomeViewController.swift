5c35aae240bcc0fc990295941b8c8b8b4adc9f6c
A Kasarin <>
2024-04-02 12:05:06 +0200

    Project init

---
+++ Mars Rover Photos/Mars Rover Photos/Module/Home/View/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  Mars Rover Photos
+//
+//  Created by Anton Kasaryn on 2.04.24.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+    }
+}
